a751c7cad50bb80df77e3803d4148ceb74e62484
Taufik Hidayat <taufikhdyt@student.ub.ac.id>
2025-02-10 21:36:40 +0700

    Update Message.jsx

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -32,7 +32,7 @@ function Message() {
             className="flex items-center justify-center gap-2 px-4 py-2 text-sm text-white border rounded-lg bg-white/20 hover:bg-white/30 backdrop-blur-sm border-white/50"
             onClick={() => navigate('/recap')}
           >
-            <ArrowLeft /> Previous page
+            <ArrowLeft /> Kembali
           </button>
         </div>
       </div>
